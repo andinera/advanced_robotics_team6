4487fbcf814e5afb5810503c6065b417a72902dd
slockwoo <shanemichaellockwood@gmail.com>
2018-03-02 09:48:44 -0700

    Added comments to set_target.py

--- set_target.py
+++ set_target.py
@@ -1,16 +1,21 @@
 import serial
 import time
-import maestro
 
-def mini_Protocol(usb):
+# Uses Pololu's Mini SSC protocol
+def mini_protocol(usb):
+    # Sweep wheels left to right
     for i in range(0, 255):
+        # Send set_target command to Pololu
         usb.write(chr(0xFF)+chr(0x00)+chr(i))
         print i
+        # Send get_position command to Pololu
         usb.write(chr(0x90)+chr(0x00))
+        # Read servo's position from Pololu
         print ord(usb.read())
         print ord(usb.read())
         print
         time.sleep(0.01)
+    # Same as above but from right to left
     for i in range(254, -1, -1):
         usb.write(chr(0xFF)+chr(0x00)+chr(i))
         print i
@@ -20,18 +25,24 @@ def mini_Protocol(usb):
         print
         time.sleep(0.01)
 
-
-def compact_Protocol(usb):
+# Uses Pololu's Compact protocol
+def compact_protocol(usb):
+    # Sweep wheels form left to right
     for i in range(4095,7906):
+        # Convert integer to high and low binary values
         low = int("{0:b}".format(i)[-7:], base=2)
         high = int("{0:b}".format(i)[0:-7], base=2)
+        # Send set_target command to Pololu
         usb.write(chr(0x84)+chr(0x00)+chr(low)+chr(high))
         print i
+        # Send get_position command to Pololu
         usb.write(chr(0x90)+chr(0x00))
+        # Read servo's position from Pololu
         print ord(usb.read())
         print ord(usb.read())
         print
         time.sleep(0.0001)
+    # Same as above but from right to left
     for i in range(7905, 4094, -1):
         low = int("{0:b}".format(i)[-7:], base=2)
         high = int("{0:b}".format(i)[0:-7], base=2)
@@ -43,12 +54,14 @@ def compact_Protocol(usb):
         print
         time.sleep(0.0001)
 
+# Main function
 if __name__ == '__main__':
+    # Setup serial connection with Pololu
     usb = serial.Serial('/dev/ttyACM0')
 
-    mini_Protocol(usb)      # Simple, less precise protocol
-    compact_Protocol(usb)   # Complex, more precise protocol
+    mini_protocol(usb)      # Simple, less precise protocol
+    compact_protocol(usb)   # Complex, more precise protocol
 
-    usb.write(chr(0xA1))
-    usb.write(chr(0xA2))
-    usb.close()
+    usb.write(chr(0xA1))    # Get errors
+    usb.write(chr(0xA2))    # Go home/clear erros
+    usb.close()             # Close connection with Pololu
--- test_pololu.py
+++
@@ -1,5 +0,0 @@
-import maestro
-
-if __name__ == '__main__':
-        pololu = maestro.Controller()
-        pololu.setTarget(0, 256)
