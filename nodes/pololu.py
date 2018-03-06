8302fd92291072c4f896c14fc4ae15284ebb7587
slockwoo <shanemichaellockwood@gmail.com>
2018-03-06 16:57:30 -0700

    Added kill switch

--- nodes/pololu.py
+++ nodes/pololu.py
@@ -15,7 +15,7 @@ import serial
 # Pololu driver
 class Controller:
 
-    usb = serial.Serial('/dev/ttyACM0')
+    # usb = serial.Serial('/dev/ttyACM0')
 
     # Pass component channel as an integer in the range [0,17] and serial port
     # as a string
@@ -24,7 +24,7 @@ class Controller:
     # This example will initialize the Controller with channel 0 and the serial
     # connection to '/dev/ttyACM0'.
     def __init__(self, channel=0, usb='/dev/ttyACM0'):
-        Controller.usb = serial.Serial(usb)
+        # Controller.usb = serial.Serial(usb)
         self.channel = channel
         self.target = 6000
 
@@ -38,6 +38,8 @@ class Controller:
 
     # This will close the usb port upon deleting this object
     def __del__(self):
+        if self.target != 6000:
+            self.set_target(6000)
         pass
 
     # Pass servo target as an integer in the range [4095, 7905]
--- nodes/test_pololu.py
+++ nodes/test_pololu.py
@@ -8,17 +8,17 @@ import csv
 
 rospy.init_node('tester')
 
-csv_out = open("ir_output.csv", 'a')
-writer = csv.writer(csv_out)
+# csv_out = open("ir_output.csv", 'a')
+# writer = csv.writer(csv_out)
 with Controller(0) as steering, Controller(1) as motor, Controller(2) as ir_one:
-	rate = rospy.Rate(10)
+	# rate = rospy.Rate(10)
 	i = 0
 	while not rospy.is_shutdown():
-		data = ir_one.get_position()
-		writer.writerow([data])
+		# data = ir_one.get_position()
+		# writer.writerow([data])
 		print i
-		rate.sleep()
+		# rate.sleep()
 		i += 1
-		if i == 50:
-			break
-csv_out.close()
+		# if i == 50:
+			# break
+# csv_out.close()
