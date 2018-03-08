051965f3e4087e3e85895be94e8819697cf3f809
carl <carlcstahoviak@gmail.com>
2018-03-08 15:38:38 -0700

    edits to pid_test.py

--- nodes/pid_test.py
+++ nodes/pid_test.py
@@ -14,35 +14,32 @@ def PIDControl(P,I,D):
     with Controller(0) as steering, Controller(1) as motor, \
          Controller(2) as ir_bottom:
 
-<<<<<<< HEAD
-	motor.set_target(CENTER)
-	time.sleep(2)
-
-        pid = PID.PID(P, I, D)
-
-        ir_bottom.get_position()
-=======
-        pid = PID.PID(P, I, D)
-        motor.set_target(6000)
+        # set zero intial velocity
+        motor.set_target(CENTER)
         time.sleep(2)
->>>>>>> b6566164ccdb9931be123829bfcd813b957f4b81
+
+        pid = PID.PID(P, I, D)
+        pid.setSampleTime(0.1)
+
         distances = []
         for i in range(1,50):
             distances.append(ir_bottom.get_position())
             time.sleep(.01)
+
+        # average of initial IR sensor data
         start_distance = int(sum(distances) / float(len(distances)))
         pid.SetPoint = start_distance
-        print start_distance #average of initial IR sensor data
-        pid.setSampleTime(0.1)
+        print "Start Distance: ",start_distance
 
+        # set forward speed
         motor.set_target(CENTER + 300)
 
         while not rospy.is_shutdown():
-
             distance = ir_bottom.get_position()
             pid.update(distance)
             output = int(pid.output) + CENTER
             print output,"\t",distance
+
             if output > MAX:
                 steering.set_target(MAX)
             elif output < MIN:
@@ -50,7 +47,10 @@ def PIDControl(P,I,D):
             else:
                 steering.set_target(output)
 
+            # how should this value relate to pid.setSampleTime?
             time.sleep(.01)
+
+        print "Start Distance: ",start_distance
 
 if __name__ == '__main__':
     rospy.init_node('odroid')
