ec48789d9b880200c6ee4a36414734e961f6d06c
Team 6 <shanemlockwood@gmail.com>
2018-03-11 10:22:26 -0600

    Renamed ros_pid_test.py

--- nodes/ros_pid_official.py
+++ nodes/ros_pid_official.py
@@ -15,22 +15,22 @@ CENTER = 6000
 
 def get_pid_control(ctrl_msg, steering):
 
-    pos_cmd = int(ctrl_msg.data)
-    pos_cmd += CENTER
-    rospy.loginfo("Position Command: %d", pos_cmd)
+    steering_cmd = int(ctrl_msg.data)
+    steering_cmd += CENTER
+    rospy.loginfo("Position Command:\t%d", steering_cmd)
 
     # send position command to sterring servo
     # (don't think this if-else structure should be necessary because of
     # upper_limit and lower_limit params set in launch file)
-    if pos_cmd > MAX:
+    if steering_cmd > MAX:
         steering.set_target(MAX)
-    elif pos_cmd < MIN:
+    elif steering_cmd < MIN:
         steering.set_target(MIN)
     else:
-        steering.set_target(pos_cmd)
+        steering.set_target(steering_cmd)
 
     # not sure if there should be a sleep command here...
-    time.sleep(.05)
+    time.sleep(.01)
 
 def pid_broadcaster():
 
@@ -75,14 +75,19 @@ def pid_broadcaster():
         print "Setpoint = ",setpoint," cm"
 
         # set forward speed
-        offset = 150
+        offset = 300
         motor.set_target(CENTER + offset)
 
         while not rospy.is_shutdown():
 
             # get position reading from IR sesnor(s)
-            position = ir_bottom.get_position()
-            rospy.loginfo("IR Position: %f", position)
+            position = []
+            for i in range(1,1):
+                position.append(ir_bottom.get_position())
+                time.sleep(0.01)
+            
+            position = sum(position) / int(len(distance))
+            rospy.loginfo("IR Position:\t%f", position)
 
             # use heading data to correct position measurement
 
@@ -90,7 +95,7 @@ def pid_broadcaster():
             state_pub.publish(state_msg)
 
             # what is this doing?
-            time.sleep(0.05)
+            time.sleep(0.01)
 
 
 if __name__ == '__main__':
