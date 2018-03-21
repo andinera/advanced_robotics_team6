02ef2fe6f890c3f50d02569fc33d1c42469ee145
slockwoo <shanemichaellockwood@gmail.com>
2018-03-21 08:08:49 -0600

    Added smach example

--- nodes/test1.py
+++ nodes/test1.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 
 import rospy
+from state_machines import example_state_machine
 
 if __name__ == '__main__':
     rospy.init_node('test_node', anonymous=True)
-    lst = [1, 2, 3, 4, 5, 4, 3, 2, 1]
-    del_lst = lst[:]
-    print lst
-    for i in lst[:]:
-        print i
-        print lst
-        lst.remove(i)
-        print lst
-        print
+    print example_state_machine.main()
