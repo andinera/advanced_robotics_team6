ade86effdee2034954455ace5337ba6e5f998834
kartner20 <eidok57@gmail.com>
2018-04-22 11:57:30 -0600

    changes

--- scripts/kodie/wall_follower.py
+++ scripts/kodie/wall_follower.py
@@ -35,7 +35,7 @@ class Wall_Follower:
         self.bottom_d_max = 700
         #drift values
         self.top_drift = 200
-        self.side_acceration_limit = 0.01
+        self.side_acceration_limit = 0.5
         #state speeds
         self.motor_speed = 6400
         self.wall_speed = 6350
@@ -48,7 +48,7 @@ class Wall_Follower:
         self.drift_wall_speed = 6900
         self.drift_speed = 4005
 
-        self.acceleration_min = 0.001
+        self.acceleration_min = 1
         self.doorways_seen_threshold = 0
         # Event for synchronizing processes
         self.event = event
@@ -76,10 +76,34 @@ class Wall_Follower:
 
         self.write_data = True
 
+        #initialze data for wall_logic
+        self.regression = linear_model.LinearRegression()
+        self.predicted_wall_distance = -1
+        self.regression_coef = -1
+        self.regression_score = None
+        #create top and bottom ir variables for simplisity
+        self.ir_top = None
+        self.ir_bottom = None
+        # define setpoint error values for state switching logic
+        self.ir_bottom_error = None
+        self.imu_wall_error = None
+        self.imu_corner_error = None
+        # finite differencing on state to estimate derivative
+        self.ir_bottom_diff = None
+        self.ir_top_diff = None
+        self.ir_top_difference = None
+        self.imu_wall_diff = None
+        self.imu_corner_diff = None
+        self.ir_bottom_average_error = None
+        #accelerameter states for simplicity
+        self.x_accel = None
+        self.y_accel = None
+        self.imu_heading = None
         # used for recording data
         if self.write_data:
             print "OPENING CSV"
-            file_name = "/home/odroid/ros_ws/src/advanced_robotics_team6/data/ir_course_data_wall{}.csv".format(time.time())
+            #need to check
+            file_name = "/home/odroid/ros_ws/src/advanced_robotics_team6/data/ir_course_data_wall{}.csv".format(time.time().second)
             csv_out = open(file_name , 'a')
             # csv_out = open("ir_course_data_doorway1.csv", 'a')
             self.writer = csv.writer(csv_out)
@@ -88,10 +112,6 @@ class Wall_Follower:
         self.top_ir_pid.ir_setpoint(setpoint=125)
         self.wall_imu_pid.imu_setpoint(states=self.cns.imu_states['orientation']['z'])
         self.corner_imu_pid.imu_setpoint(setpoint=self.wall_imu_pid.setpoint.data)
-
-
-        #self.motor_srv(6450)
-        #rospy.sleep(0.5)
 
         # Set forward speed
         self.motor_srv(6300)
@@ -157,7 +177,8 @@ class Wall_Follower:
                 self.regression = linear_model.LinearRegression()
                 self.regression.fit(x_range, top_ir_states)
                 self.predicted_wall_distance =  self.regression.predict([NUM_RECORDED_STATES])
-                #self.regression_score = self.regression.score()
+                self.regression_coef = self.regression.coef_
+                self.regression_score = self.regression.score(x_range, top_ir_states)
                 self.do_regression = True
             #create top and bottom ir variables for simplisity
             self.ir_top = self.top_ir_pid.state.data
@@ -175,8 +196,8 @@ class Wall_Follower:
 
             self.ir_bottom_average_error = math.fabs(self.bottom_ir_pid.setpoint.data - (self.bottom_ir_pid.reported_states[-1] + self.bottom_ir_pid.reported_states[-2] + self.bottom_ir_pid.reported_states[-3])/3)
             #accelerameter states for simplicity
-            self.x_accel = self.cns.imu_states['linear_acceleration']['x'][-1]
-            self.y_accel = self.cns.imu_states['linear_acceleration']['y'][-1]
+            self.x_accel = self.cns.imu_states['linear_acceleration']['x'][-1][0] #need to check this
+            self.y_accel = self.cns.imu_states['linear_acceleration']['y'][-1][0] # need to check this too
             self.imu_heading = self.corner_imu_pid.state.data
 
             #print all useful information
@@ -195,22 +216,22 @@ class Wall_Follower:
                 rospy.loginfo("Regression Coef:\t%f", self.regression.coef_)
                 rospy.loginfo("Predicted Wall Distance:\t%f",self.predicted_wall_distance )
                 #rospy.loginfo("Regression Score:\t%f",self.regression_score)
-            if self.write_data and self.do_regression:
+            if self.write_data:
                 self.writer.writerow([time.time(),self.state,self.stage,self.ir_bottom,\
                  self.ir_bottom_error, self.ir_bottom_diff, self.ir_top, self.ir_top_diff,\
                   self.imu_heading, self.imu_corner_error, self.x_accel, self.y_accel,\
-                   self.regression.coef_, self.predicted_wall_distance])
-            elif self.write_data:
-                self.writer.writerow([time.time(),self.state,self.stage,self.ir_bottom,\
-                 self.ir_bottom_error, self.ir_bottom_diff, self.ir_top, self.ir_top_diff,\
-                  self.imu_heading, self.imu_corner_error, self.x_accel, self.y_accel])
+                   self.regression_coef, self.regression_score, self.predicted_wall_distance])
             if self.state == 'data':
                 if time.time() - self.data_timer > 3:
                     self.motor_srv(MOTOR_CENTER)
                     self.write_data = False
             elif self.state == 'wall_follow':
+                #check drift
+                if self.start_drift_config():
+                    print "DRIFT"
+                    self.start_drift_config()
                 #check corner near state
-                if self.cornernear_logic():
+                elif self.cornernear_logic():
                     print "CORNER NEAR"
                     self.cornernear_config()
                 #check for corner detection
@@ -253,7 +274,7 @@ class Wall_Follower:
                     self.wall_config()
                     print "Exited corner near with standard method"
                 #car has stoped to enter corner near stopped state
-                elif math.fabs(self.x_accel) < self.acceleration_min:
+            elif math.fabs(self.y_accel) < self.acceleration_min:
         		    self.state = 'corner_near_stopped'
 
             elif self.state == 'corner_near_stopped':
@@ -349,7 +370,7 @@ class Wall_Follower:
         else:
             return False
     def enddrift_logic(self):
-        if self.y_accel < self.side_acceration_limit:
+        if self.x_accel < self.side_acceration_limit:
             return True
         else:
             return False
