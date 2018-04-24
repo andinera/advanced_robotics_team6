9c609308a5bbf9634b6a4f6b655a6bac12e17516
team6robot <shanemlockwood@gmail.com>
2018-04-24 18:11:48 +0000

    odom msg added

---
+++ nodes/imu_odometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import rospy
+
+from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
+
+rospy.init_node('imu_odometry', anonymous=True)
+
+def imu_callback(data, pub):
+    x = data.orientation.x
+    y = data.orientation.y
+    z = data.orientation.z
+    w = data.orientation.w
+
+    odometry = Odometry()
+    odometry.header = data.header
+    odometry.pose.pose.orientation.x = x
+    odometry.pose.pose.orientation.y = y
+    odometry.pose.pose.orientation.z = z
+    odometry.pose.pose.orientation.w = w
+    pub.publish(odometry)
+
+imu_pub = rospy.Publisher("imu/odometry",
+        	                               Odometry,
+                	                       queue_size=1)
+
+imu_sub = rospy.Subscriber("imu/data",
+                                        Imu,
+                                        imu_callback,
+										imu_pub)
+
+rospy.spin()
