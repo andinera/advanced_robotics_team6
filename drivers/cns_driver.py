61b67d97a8ec54eac90f553b4600d85c949e6b44
Shane Lockwood <shanemichaellockwood@gmail.com>
2018-04-28 17:52:13 -0600

    Reverted other nodes

--- drivers/cns_driver.py
+++ drivers/cns_driver.py
@@ -1,102 +1,101 @@
-#!/usr/bin/env python
-
 import rospy
+from multiprocessing import Manager
 
 from sensor_msgs.msg import Imu
 from std_msgs.msg import Float64
-from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
-from tf.transformations import quaternion_from_euler
 
 from advanced_robotics_team6.srv import PololuCmd
 
 
+NUM_READINGS = 1500
+
 class CNS:
 
     def __init__(self):
-        # Maximum length of each data list
-        self.num_readings = 151
         # Setup data structures for saving sensor input data which can be shared
         # across processes
-        self.ir_one_states = []
-        self.ir_two_states = []
-        self.imu_states = {'orientation': {'x': [],
-                                           'y': [],
-                                           'z': []},
-                           'angular_velocity': {'x': [],
-                                                'y': [],
-                                                'z': []},
-                           'linear_acceleration': {'x': [],
-                                                   'y': [],
-                                                   'z': []}}
+        self.bottom_ir_states = Manager().list()
+        self.top_ir_states = Manager().list()
+        self.imu_states = {'orientation': {'x': Manager().list(),
+                                           'y': Manager().list(),
+                                           'z': Manager().list()},
+                           'angular_velocity': {'x': Manager().list(),
+                                                'y': Manager().list(),
+                                                'z': Manager().list()},
+                           'linear_acceleration': {'x': Manager().list(),
+                                                   'y': Manager().list(),
+                                                   'z': Manager().list()}}
+
         # Initialize services for sending motor and steering commands
         rospy.wait_for_service('motor_cmd')
         rospy.wait_for_service('steering_cmd')
-        self.motor_srv = rospy.ServiceProxy('motor_cmd',
-                                            PololuCmd)
-        self.steering_srv = rospy.ServiceProxy('steering_cmd',
-                                               PololuCmd)
-        # Intialize subscriber for IR sensor data
-        self.ir_one_sub = rospy.Subscriber("pololu/ir/one/data",
-                                           Float64,
-                                           self.ir_one_callback)
-        self.ir_two_sub = rospy.Subscriber("pololu/ir/two/data",
-                                           Float64,
-                                           self.ir_two_callback)
-        # Initialize subscriber for IMU data
+        self.motor_srv = rospy.ServiceProxy('motor_cmd', PololuCmd)
+        self.steering_srv = rospy.ServiceProxy('steering_cmd', PololuCmd)
+
+        # Intialize subscriber for bottom IR sensor
+        self.bottom_ir_sub = rospy.Subscriber("pololu/ir/bottom/data",
+                                          Float64,
+                                          self.bottom_ir_callback)
+
+        # Initialize subscriber for top IR sensor
+        self.top_ir_sub = rospy.Subscriber("pololu/ir/top/data",
+                                      Float64,
+                                      self.top_ir_callback)
+
+        # Initialize subscriber for IMU
         self.imu_sub = rospy.Subscriber("imu/data",
                                         Imu,
                                         self.imu_callback)
         # Sleep to allow the sensor data structures to populate
         rospy.sleep(0.25)
 
-    # Callback for recording data from IR sensor one
-    def ir_one_callback(self, data):
-        if len(self.ir_one_states) >= self.num_readings:
-            del self.ir_one_states[0]
-        self.ir_one_states.append(data.data)
+    # Callbacks for recording data from top IR sensor
+    def bottom_ir_callback(self, data):
+        if len(self.bottom_ir_states) >= NUM_READINGS:
+            del self.bottom_ir_states[0]
+        self.bottom_ir_states.append(data.data)
 
-    # Callback for recording data from IR sensor two
-    def ir_two_callback(self, data):
-        if len(self.ir_two_states) >= self.num_readings:
-            del self.ir_two_states[0]
-        self.ir_two_states.append(data.data)
+    # Callbacks for recording data from top IR sensor
+    def top_ir_callback(self, data):
+        if len(self.top_ir_states) >= NUM_READINGS:
+            del self.top_ir_states[0]
+        self.top_ir_states.append(data.data)
 
-    # Callback for recording data from IMU
+    # Callbacks for recording data from IMU
     def imu_callback(self, data):
         x = data.orientation.x
         y = data.orientation.y
         z = data.orientation.z
         w = data.orientation.w
         angles = euler_from_quaternion([x, y, z, w])
-        #quat = quaternion_from_euler(angles[0],angles[1],angles[2])
-        #print "quaternion:", quat
-        if len(self.imu_states['orientation']['x']) >= self.num_readings:
+
+        if len(self.imu_states['orientation']['x']) >= NUM_READINGS:
             del self.imu_states['orientation']['x'][0]
         self.imu_states['orientation']['x'].append(angles[0])
-        if len(self.imu_states['orientation']['y']) >= self.num_readings:
+        if len(self.imu_states['orientation']['y']) >= NUM_READINGS:
             del self.imu_states['orientation']['y'][0]
         self.imu_states['orientation']['y'].append(angles[1])
-        if len(self.imu_states['orientation']['z']) >= self.num_readings:
+        if len(self.imu_states['orientation']['z']) >= NUM_READINGS:
             del self.imu_states['orientation']['z'][0]
         self.imu_states['orientation']['z'].append(angles[2])
 
-        if len(self.imu_states['angular_velocity']['x']) >= self.num_readings:
+        if len(self.imu_states['angular_velocity']['x']) >= NUM_READINGS:
             del self.imu_states['angular_velocity']['x'][0]
         self.imu_states['angular_velocity']['x'].append(data.angular_velocity.x)
-        if len(self.imu_states['angular_velocity']['y']) >= self.num_readings:
+        if len(self.imu_states['angular_velocity']['y']) >= NUM_READINGS:
             del self.imu_states['angular_velocity']['y'][0]
         self.imu_states['angular_velocity']['y'].append(data.angular_velocity.y)
-        if len(self.imu_states['angular_velocity']['z']) >= self.num_readings:
+        if len(self.imu_states['angular_velocity']['z']) >= NUM_READINGS:
             del self.imu_states['angular_velocity']['z'][0]
         self.imu_states['angular_velocity']['z'].append(data.angular_velocity.z)
 
-        if len(self.imu_states['linear_acceleration']['x']) >= self.num_readings:
+        if len(self.imu_states['linear_acceleration']['x']) >= NUM_READINGS:
             del self.imu_states['linear_acceleration']['x'][0]
         self.imu_states['linear_acceleration']['x'].append(data.linear_acceleration.x)
-        if len(self.imu_states['linear_acceleration']['y']) >= self.num_readings:
+        if len(self.imu_states['linear_acceleration']['y']) >= NUM_READINGS:
             del self.imu_states['linear_acceleration']['y'][0]
         self.imu_states['linear_acceleration']['y'].append(data.linear_acceleration.x)
-        if len(self.imu_states['linear_acceleration']['z']) >= self.num_readings:
+        if len(self.imu_states['linear_acceleration']['z']) >= NUM_READINGS:
             del self.imu_states['linear_acceleration']['z'][0]
-        self.imu_states['linear_acceleration']['z'].append(data.linear_acceleration.x)
+self.imu_states['linear_acceleration']['z'].append(data.linear_acceleration.x)
--- nodes/wall_follow.py
+++ nodes/wall_follow.py
@@ -8,45 +8,40 @@ from threading import Thread, Event
 from advanced_robotics_team6.scripts import *
 
 
-class WallFollow:
-
-    def __init__(self):
-        # Retrieve dev parameter from parameter server
-        self.dev = rospy.get_param('~dev')
-        # Frequency for iteration
-        self.frequency = 50
-        # Event for synchronizing child scripts
-        self.event = Event()
-        # Initialize Wall_Follower script
-        try:
-            self.wall_follower = globals()[self.dev].wall_follower.Wall_Follower(self.event)
-        except KeyError, e:
-            print "Developer not specified:", e
-            nodes = os.popen('rosnode list').readlines()
-            for i in range(len(nodes)):
-                nodes[i] = nodes[i].replace("\n","")
-            for node in nodes:
-                os.system("rosnode kill "+ node)
-            sys.exit()
-        # Initialize timer to iterate at set frequency
-        self.timer = rospy.get_rostime()
-
-    def iterate(self):
-        # Run state machine
-        Thread(target=self.wall_follower.execute).start()
-        # Begin iterations at set freuqency
-        while not rospy.is_shutdown():
-            # Iterate at set frequency
-            while not rospy.is_shutdown() and self.timer > rospy.get_rostime():
-                rospy.sleep(0.1/self.frequency)
-            # Reset freuqency timer
-            self.timer += rospy.Duration(1.0/self.frequency)
-            # Allow processes waiting on event to run
-            self.event.set()
-        # Perform any necessary cleaning up before ending script
-        self.wall_follower.finish()
+FREQUENCY = 50
 
 if __name__ == '__main__':
+    # Initialize node
     rospy.init_node('wall_follow_node', anonymous=True)
-    wf = WallFollow()
-    wf.iterate()
+    DEV = rospy.get_param('~dev')
+
+    event = Event()
+
+    # Initialize Wall_Follower
+    try:
+        wall_follower = globals()[DEV].wall_follower.Wall_Follower(event)
+    except KeyError, e:
+        print "Developer not specified:", e
+        nodes = os.popen('rosnode list').readlines()
+        for i in range(len(nodes)):
+            nodes[i] = nodes[i].replace("\n","")
+        for node in nodes:
+            os.system("rosnode kill "+ node)
+        sys.exit()
+
+    # Run state machine
+    Thread(target=wall_follower.execute).start()
+    # wall_follower.execute()
+    # Initialize timer to iterate at set frequency
+    timer = rospy.get_rostime() + rospy.Duration(1.0/FREQUENCY)
+    # Begin iterations at set freuqency
+    while not rospy.is_shutdown():
+        # Allow processes waiting on event to run
+        event.set()
+        # Iterate at set frequency
+        while not rospy.is_shutdown() and timer > rospy.get_rostime():
+            rospy.sleep(0.1/FREQUENCY)
+        # Reset freuqency timer
+        timer += rospy.Duration(1.0/FREQUENCY)
+    # Perform any necessary cleaning up before ending script
+wall_follower.finish()
